5c81a26903abb3b45e26c8e4fb6bfd0262798f1f
Juho Jaakkola <juho.jaakkola@mediamaisteri.com>
2013-08-01 17:20:38 +0300

    Added translations for CKEditor plugin.

---
+++ ckeditor/languages/fi.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * CKEditor language pack.
+ *
+ * @package ElggCKEditor
+ */
+
+return array(
+	'admin:administer_utilities:uploads' => 'CKEditorin tiedostot',
+	'ckeditor:uploads:none' => 'Ei tiedostoja',
+	'item:object:ckeditor_upload' => 'CKEditorilla ladatut tiedostot',
+	'ckeditor:upload:owner' => 'Käyttäjä: %s',
+	'ckeditor:upload:admin' => 'Tiedostojen hallinta',
+
+	'ckeditor:remove' => "Piilota editori",
+	'ckeditor:add' => "Näytä editori",
+	'ckeditor:word_count' => 'Sanamäärä: ',
+
+	'ckeditor:success:delete' => 'Tiedosto poistettu',
+	'ckeditor:failure:upload' => 'Kuvan lisääminen epäonnistuis',
+	'ckeditor:failure:resize' => 'Kuvan koon muuttaminen epäonnistui',
+	'ckeditor:failure:permissions' => 'Virhe palvelimen asetuksissa',
+	'ckeditor:failure:too_big' => 'Kuva on liian iso. Pienennä sitä ennen lisäämistä.',
+	'ckeditor:failure:missing' => 'Tiedostoa ei löydy',
+	'ckeditor:failure:delete' => 'Poistaminen epäonnistui',
+);
