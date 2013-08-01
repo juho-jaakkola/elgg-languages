b1608e64ea955b6bfd9b9a23923081afea4f73e7
Juho Jaakkola <juho.jaakkola@mediamaisteri.com>
2013-08-01 17:54:41 +0300

    Added translations for site_notifications plugin.

---
+++ site_notifications/languages/fi.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Site notifications Finnish language file
+ */
+
+return array(
+	'site_notifications' => 'Ilmoitukset',
+	'notification:method:site' => 'Ilmoitukset',
+	'site_notifications:topbar' => 'Ilmoitukset',
+	'item:object:site_notification' => 'Ilmoitukset',
+	'site_notifications:view_all' => 'Näytä kaikki',
+
+	'site_notifications:no_access' => 'Sinulla ei ole oikeuksia tälle sivulle. Sinun pitää ehkä kirjautua sisään.',
+	'site_notifications:no_delete' => 'Poistaminen epäonnistui',
+	'site_notifications:cannot_set_read' => "Tämä ilmoitus on poistettu tai sinulla ei ole oikeuksia sen näkemiseen.",
+	'site_notifications:empty' => 'Ei ilmoituksia',
+);
